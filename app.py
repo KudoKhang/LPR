3acb1d8611c29a4339df896bb3c17261db98446d
KudoKhang <thanhchem.k39a1@gmail.com>
2022-09-13 16:34:07 +0700

    server

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import RedirectResponse
 from io import BytesIO
+import io
 from PIL import Image
 
 def read_image_file(file) -> Image.Image:
     image = Image.open(BytesIO(file))
     image = np.array(image)[:,:,::-1]
-    return image
 
 LPRPredictor = LicensePlateRecognition()
 
@@ -20,17 +20,15 @@ app = FastAPI(title="Lisence Plate Recognition", description=app_desc)
 async def index():
     return RedirectResponse(url="/docs")
 
-@app.post("/predict/image")
+@app.post("/predict/")
 async def predict_api(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
-
     image = read_image_file(await file.read())
     prediction = LPRPredictor.predict(image)
-    # print(prediction)
+    print(prediction)
     return prediction
-
 
 if __name__ == "__main__":
     uvicorn.run(app, debug=True)
